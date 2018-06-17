09482787d77353d0eb5d1d8d6139b29e43779a21
VietKhang <1511465@hcmut.edu.vn>
2018-06-17 19:57:39 +0700

    Ok

--- YDKJS.js
+++ YDKJS.js
@@ -1,6 +1,10 @@
-function foo() {
-    "use strict";
-    console.log(this.a);
+function foo(something) {
+    console.log(this.a, something);
+    return this.a + something;
 }
-var a = 2;
-foo();
+var obj = {
+    a : 2
+}
+var bar = foo.bind(obj);
+var b = bar(3);
+console.log(b);
