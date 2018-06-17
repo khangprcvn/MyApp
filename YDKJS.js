006e0d0a70d918644af86192c50032e145a66df4
VietKhang <1511465@hcmut.edu.vn>
2018-06-17 18:21:40 +0700

    Upload

--- YDKJS.js
+++ YDKJS.js
@@ -1,22 +1,6 @@
-// function foo() {
-//     // "use strict";
-//     console.log(this.a);
-// }
-// var obj = {
-//     a : 2,
-//     foo : foo
-// };
-// obj.foo();
-
-function baz() {
-    console.log("baz");
-    bar();
-}
-function bar() {
-    console.log("bar");
-    foo();
-}
 function foo() {
-    console.log("foo");
+    "use strict";
+    console.log(this.a);
 }
-baz();
+var a = 2;
+foo();
